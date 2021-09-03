25a3d2db3c0b97da56b79c2b94b3557e834446a4
Lior Nitzan <nitzanlior1968@gmail.com>
2021-09-03 17:52:38 +0200

    basic installation

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+// const runtimeCaching = require('./cache');
+// const i18n = require('./next-i18next.config').i18n;
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+});
