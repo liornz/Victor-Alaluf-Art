c8ba964e5f0339eadb08ceb84b2e3e556152112b
Lior Nitzan <nitzanlior1968@gmail.com>
2021-11-05 22:12:30 +0100

    fix background image JPG to jpg and workaround issue with pwa manifest

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-// const runtimeCaching = require('./cache');
+// const runtimeCaching = require('next-pwa/cache');
+const runtimeCaching = require('./cache');
 const { i18n } = require('./next-i18next.config');
 
 module.exports = withPWA({
   pwa: {
     dest: 'public',
     runtimeCaching,
+    buildExcludes: [/middleware-manifest.json$/],
   },
   images: {
     formats: ['image/avif', 'image/webp'],
